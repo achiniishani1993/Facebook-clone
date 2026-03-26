c31472773d269db5bf0d6a2e77b2094f9969f43b
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-26 09:52:01 +0100

    updated avBar.tsx file

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,4 +1,7 @@
 import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import MessageIcon from '@mui/icons-material/Message';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export const Navbar = () => {
   return (
@@ -9,9 +12,32 @@ export const Navbar = () => {
         <div className="navBarCenter">
             <div className="searchBarBox">
                 <SearchIcon className='searchicon'/>
+                <input placeholder='search friends' className="searchinput" />
             </div>
         </div>
-        <div className="navBarRight"></div>
+        <div className="navBarRight">
+            <div className="navBarLinks">
+                <span className="navBarLink">HomePage</span>
+                <span className="navBarLink">Profile</span>
+            </div>
+            <div className="navBarIcons">
+                <div className="navBarIcon">
+                    <PersonIcon/>
+                    <span className="iconTag">3</span>
+                </div>
+                 <div className="navBarIcon">
+                    <MessageIcon/>
+                     <span className="iconTag">5</span>
+                 </div>
+                  <div className="navBarIcon">
+                    <SettingsIcon />
+                     <span className="iconTag">1</span>
+                  </div>
+            </div>
+            <div className="pic">
+                <img src="/Images/1.jpeg" alt="profile image" className="profilePic" />
+            </div>
+        </div>
     </div>
   )
 }
