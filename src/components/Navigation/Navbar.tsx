f2459582031e9fd331f31a6693e8b95f80ed6725
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-26 11:19:43 +0100

    add css to navigation

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -2,6 +2,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import MessageIcon from '@mui/icons-material/Message';
 import SettingsIcon from '@mui/icons-material/Settings';
+import "./navbar.css"
 
 export const Navbar = () => {
   return (
@@ -11,8 +12,8 @@ export const Navbar = () => {
         </div>
         <div className="navBarCenter">
             <div className="searchBarBox">
-                <SearchIcon className='searchicon'/>
-                <input placeholder='search friends' className="searchinput" />
+                <SearchIcon className='searchIcon'/>
+                <input placeholder='Search for your friends' className="searchinput" />
             </div>
         </div>
         <div className="navBarRight">
