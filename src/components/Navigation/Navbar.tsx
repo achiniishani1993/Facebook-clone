02cb804a9152a12d9efea032649883b2cba4fe81
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-26 11:29:42 +0100

    add font for the logo

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,44 +1,51 @@
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import MessageIcon from '@mui/icons-material/Message';
-import SettingsIcon from '@mui/icons-material/Settings';
-import "./navbar.css"
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import MessageIcon from "@mui/icons-material/Message";
+import SettingsIcon from "@mui/icons-material/Settings";
+import "./navbar.css";
 
 export const Navbar = () => {
   return (
     <div className="navBarbox">
-        <div className="navBarLeft">
-            <span className="log">FaceBook</span>
+      <div className="navBarLeft">
+        <span className="log">faceBook</span>
+      </div>
+      <div className="navBarCenter">
+        <div className="searchBarBox">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Search for your friends"
+            className="searchinput"
+          />
         </div>
-        <div className="navBarCenter">
-            <div className="searchBarBox">
-                <SearchIcon className='searchIcon'/>
-                <input placeholder='Search for your friends' className="searchinput" />
-            </div>
+      </div>
+      <div className="navBarRight">
+        <div className="navBarLinks">
+          <span className="navBarLink">HomePage</span>
+          <span className="navBarLink">Profile</span>
         </div>
-        <div className="navBarRight">
-            <div className="navBarLinks">
-                <span className="navBarLink">HomePage</span>
-                <span className="navBarLink">Profile</span>
-            </div>
-            <div className="navBarIcons">
-                <div className="navBarIcon">
-                    <PersonIcon/>
-                    <span className="iconTag">3</span>
-                </div>
-                 <div className="navBarIcon">
-                    <MessageIcon/>
-                     <span className="iconTag">5</span>
-                 </div>
-                  <div className="navBarIcon">
-                    <SettingsIcon />
-                     <span className="iconTag">1</span>
-                  </div>
-            </div>
-            <div className="pic">
-                <img src="/Images/1.jpeg" alt="profile image" className="profilePic" />
-            </div>
+        <div className="navBarIcons">
+          <div className="navBarIcon">
+            <PersonIcon />
+            <span className="iconTag">3</span>
+          </div>
+          <div className="navBarIcon">
+            <MessageIcon />
+            <span className="iconTag">5</span>
+          </div>
+          <div className="navBarIcon">
+            <SettingsIcon />
+            <span className="iconTag">1</span>
+          </div>
         </div>
+        <div className="pic">
+          <img
+            src="/Images/1.jpeg"
+            alt="profile image"
+            className="profilePic"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
