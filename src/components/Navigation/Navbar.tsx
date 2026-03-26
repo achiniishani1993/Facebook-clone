20f7e0f849de23c419291d768ed99a2dc86e7713
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-26 09:22:54 +0100

    installed material ui icon

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,7 +1,17 @@
-
+import SearchIcon from '@mui/icons-material/Search';
 
 export const Navbar = () => {
   return (
-    <div>This is navigation Bar</div>
+    <div className="navBarbox">
+        <div className="navBarLeft">
+            <span className="log">FaceBook</span>
+        </div>
+        <div className="navBarCenter">
+            <div className="searchBarBox">
+                <SearchIcon className='searchicon'/>
+            </div>
+        </div>
+        <div className="navBarRight"></div>
+    </div>
   )
 }
